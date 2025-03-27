751686c5e0dc8ebd39917876ce6585274091545e
CamilaFerrreyra <ferreyracamila1321@gmail.com>
2025-03-27 15:22:11 -0300

    modifico nombres

---
+++ practica0/programita_9g.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  char textoA[30] = "Agarrate Cataaa";
+  char textoB[30] = "El Cuarteto de Nos";
+  char *p = textoA;
+  char *q = textoB;
+  printf("textoA: %s\ntextoB: %s\n", textoA, textoB);
+
+  while (*p++ = *q++);
+
+  printf("while(*p++ = *q++);\n");
+  printf("textoA: %s\ntextoB: %s\n", textoA, textoB);
+
+  return 0;
+}
+
+/*Sin importar el tamaño de los arreglos,
+el while copia el segundo en el primero.*/
