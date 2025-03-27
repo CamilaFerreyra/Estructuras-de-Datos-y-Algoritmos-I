6e92a0829bc59f4ba8272e247fbc4bbd270948bb
CamilaFerrreyra <ferreyracamila1321@gmail.com>
2025-03-27 14:47:28 -0300

    agrego programita, ejemplo de cosaas que no se deben hacer

---
+++ practica0/programita.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int *direccion_local(int x) {
+    int *p;
+    p = &x;
+    return p;
+    //retunr &x; está mal mal muy mal.
+}
+
+int main() {
+    int *prt = NULL;
+    int num = 2;
+    prt = direccion_local(num);
+    printf("%d\n", *prt);
+    return 0;
+}
