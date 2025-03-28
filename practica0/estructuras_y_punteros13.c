ea2296a88d2f1d288c63155b5af82ccd8742bcea
CamilaFerrreyra <ferreyracamila1321@gmail.com>
2025-03-28 09:37:53 -0300

    para llenar la estructura mazo es necesario usar un puntero a la estructura. Las cartas se llenan bien porque son un array, un puntero de tipo Carta

--- practica0/estructuras_y_punteros13.c
+++ practica0/estructuras_y_punteros13.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct {
   int palo;                     // 0, 1, 2, 3
@@ -7,55 +8,77 @@ typedef struct {
 } Carta;
 
 typedef struct {
-  Carta cartas[47];
+  Carta cartas[48];
 } Mazo;
 
-void llenar(Mazo);
+void llenar(Mazo *);
 void mostrar(Mazo);
 Carta azar(Mazo);
 
 int main() {
   Mazo mazo;
-  llenar(mazo);
+  llenar(&mazo);
   mostrar(mazo);
+
+  srand(time(NULL));
+
+  Carta cartaSeleccionada = azar(mazo);
+  //muestro la carta seleccionada
+  printf("Carta seleccionada aleatoriamente: \n");
+  switch (cartaSeleccionada.palo) {
+  case 0:
+    printf("Basto - ");
+    break;
+  case 1:
+    printf("Corazón - ");
+    break;
+  case 2:
+    printf("Trébol - ");
+    break;
+  case 3:
+    printf("Pica - ");
+    break;
+  default:
+    break;
+  }
+
+  printf("%d\n", cartaSeleccionada.numero);
+
   return 0;
 }
 
-void llenar(Mazo mazo) {
+void llenar(Mazo * mazo) {
   for (int i = 0, c = 0; i < 4; i++) {
     for (int j = 1; j <= 12; j++, c++) {
-
-      mazo.cartas[c].palo = i;
-      mazo.cartas[c].numero = j;
+      mazo->cartas[c].palo = i;
+      mazo->cartas[c].numero = j;
     }
   }
 }
 
 void mostrar(Mazo mazo) {
-  for (int i = 0, c = 0; i < 4; i++) {
-    for (int j = 0; j <= 12; j++, c++) {
-      switch (mazo.cartas[c].palo) {
-      case 0:
-        printf("BASTO - ");
-        break;
+  for (int c = 0; c < 48; c++) {
+    switch (mazo.cartas[c].palo) {
+    case 0:
+      printf("BASTO - ");
+      break;
 
-      case 1:
-        printf("CORAZÓN - ");
-        break;
+    case 1:
+      printf("CORAZÓN - ");
+      break;
 
-      case 2:
-        printf("TRÉBOL - ");
-        break;
+    case 2:
+      printf("TRÉBOL - ");
+      break;
 
-      case 3:
-        printf("PICA - ");
-        break;
-      default:
-        break;
-      }
-      printf("%d\n", mazo.cartas[c].numero);
+    case 3:
+      printf("PICA - ");
+      break;
+
+    default:
+      break;
     }
-
+    printf("%d\n", mazo.cartas[c].numero);
   }
 }
 
