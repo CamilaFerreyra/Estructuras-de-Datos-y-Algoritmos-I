b212a5c362f5210a117d7e496b769ebe86bfc4b3
CamilaFerrreyra <ferreyracamila1321@gmail.com>
2025-04-06 23:58:47 -0300

    nace dlist

---
+++ practica2-listas/dlist/dlist.c
@@ -0,0 +1,4 @@
+#include "dlist.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
---
+++ practica2-listas/dlist/dlist.h
@@ -0,0 +1,6 @@
+#ifndef __DLIST_H__
+#define __DLIST_H__
+
+#include <stddef.h>
+
+#endif                          /* __DLIST_H__ */
